b8293d6ba849a8eabc188f6f1f6823cbac02e643
Kirill Nerush <97055285+NerushKirill@users.noreply.github.com>
2023-10-18 21:41:03 +0300

    Update sql_ext1.sql

--- SB/sql_ext1.sql
+++ SB/sql_ext1.sql
@@ -7,10 +7,8 @@
  0, 1, 1, 2, 3, 5, 8, 13, ..., 89, 144, 233, 377, ...
 */
 
-SELECT
-  concat('0, ', group_concat(f separator ', '), ', ...') as 'Последовательность Фиббоначчи:'
+SELECT concat('0, ', group_concat(f separator ', '), ', ...') as 'Последовательность Фиббоначчи:'
 FROM (
-  SELECT
-    @f := @i + @j as f, @i := @j, @j := @f
-  FROM
-    information_schema.tables, (SELECT @i := 1, @j := 0) _ LIMIT 90) _
+  SELECT @f := @i + @j as f, @i := @j, @j := @f
+  FROM information_schema.tables, (SELECT @i := 1, @j := 0) _ LIMIT 90
+ ) _
